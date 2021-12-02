a40162904610443f314ebf105d9bc4d86ca9ab26
MarkSinke <m.j.sinke@gmail.com>
2021-12-02 17:11:54 +0100

    fix Haskell warnings for day1 and day2

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,13 @@ import Lib (day1a, day1b, day2a, day2b)
 
 main :: IO ()
 main = do
-  print "Day 2 a"
+  putStrLn "Day 2 a"
   r1 <- day2a
   print r1
+  putStr "Answer: "
+  print (case r1 of (hor, depth) -> hor * depth)
   r2 <- day2b
-  print "Day 2 b"
+  putStrLn "Day 2 b"
   print r2
+  putStr "Answer: "
   print (case r2 of (hor, depth, aim) -> hor * depth)
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,21 +4,25 @@ module Lib
 
 -- DAY1
 
+day1a :: IO Int
 day1a = do
   nums <- getInts "/Users/marksinke/IdeaProjects/aoc2021/data/day1input.txt"
-  let pairs = (zip nums (drop 1 nums))
+  let pairs = zip nums (drop 1 nums)
   return (length (filter increasing pairs))
 
+day1b :: IO Int
 day1b = do
   nums <- getInts "/Users/marksinke/IdeaProjects/aoc2021/data/day1input.txt"
-  let windows = (zip3 nums (drop 1 nums) (drop 2 nums))
+  let windows = zip3 nums (drop 1 nums) (drop 2 nums)
   let sums = map sum3 windows
-  let pairs = (zip sums (drop 1 sums))
+  let pairs = zip sums (drop 1 sums)
   return (length (filter increasing pairs))
 
+sum3 :: (Int, Int, Int) -> Int
 sum3 (a, b, c) =
   a + b + c
 
+increasing :: (Int, Int) -> Bool
 increasing (a, b) =
   a < b
 
@@ -30,15 +34,17 @@ getInts path = do
 
 -- DAY2
 
+day2a :: IO (Int, Int)
 day2a = do
   commands <- getCommands "/Users/marksinke/IdeaProjects/aoc2021/data/day2input.txt"
   let initialPos = (0, 0)
-  return (foldl applyCommandA (initialPos) commands)
+  return (foldl applyCommandA initialPos commands)
 
+day2b :: IO (Int, Int, Int)
 day2b = do
   commands <- getCommands "/Users/marksinke/IdeaProjects/aoc2021/data/day2input.txt"
   let initialPos = (0, 0, 0)
-  return (foldl applyCommandB (initialPos) commands)
+  return (foldl applyCommandB initialPos commands)
 
 applyCommandA :: (Int, Int) -> (String, Int) -> (Int, Int)
 applyCommandA (hor, depth) command =
@@ -48,6 +54,7 @@ applyCommandA (hor, depth) command =
     "backward" -> (hor - num, depth)
     "up" -> (hor, depth - num)
     "down" -> (hor, depth + num)
+    _ -> error "failed to parse input"
 
 applyCommandB :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
 applyCommandB (hor, depth, aim) command =
@@ -57,6 +64,7 @@ applyCommandB (hor, depth, aim) command =
     "backward" -> (hor - num, depth + aim * num, aim)
     "up" -> (hor, depth, aim - num)
     "down" -> (hor, depth, aim + num)
+    _ -> error "failed to parse input"
 
 getCommands :: FilePath -> IO [(String, Int)]
 getCommands path = do
@@ -66,5 +74,6 @@ getCommands path = do
 
 readCommand :: String -> (String, Int)
 readCommand str =
-  let command : numStr : _ = words str
-  in (command, read numStr)
+  case words str of
+     command : numStr : _ -> (command, read numStr)
+     _ -> error "failed to parse input"
