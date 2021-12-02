1f230d9cdade1f8f8dbc072d9cccede4cebb3cb1
MarkSinke <m.j.sinke@gmail.com>
2021-12-02 11:53:05 +0100

    day 1a and 1b working

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Lib (day1)
+import Lib (day1a, day1b)
 
 main :: IO ()
 main = do
-  r <- day1
-  print r
+  r1 <- day1a
+  print r1
+  r2 <- day1b
+  print r2
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,21 @@
 module Lib
-    ( day1
+    ( day1a, day1b
     ) where
 
-day1 = do
+day1a = do
   nums <- getInts "/Users/marksinke/IdeaProjects/aoc2021/data/day1input.txt"
   let pairs = (zip nums (drop 1 nums))
   return (length (filter increasing pairs))
+
+day1b = do
+  nums <- getInts "/Users/marksinke/IdeaProjects/aoc2021/data/day1input.txt"
+  let windows = (zip3 nums (drop 1 nums) (drop 2 nums))
+  let sums = map sum3 windows
+  let pairs = (zip sums (drop 1 sums))
+  return (length (filter increasing pairs))
+
+sum3 (a, b, c) =
+  a + b + c
 
 increasing :: (Int, Int ) -> Bool
 increasing tuple =
