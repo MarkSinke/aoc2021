fe45dfccd30c1578e16a074f22e2658094e80ed8
MarkSinke <m.j.sinke@gmail.com>
2021-12-02 14:21:29 +0100

    print headers for day 2

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,10 @@ import Lib (day1a, day1b, day2a, day2b)
 
 main :: IO ()
 main = do
+  print "Day 2 a"
   r1 <- day2a
   print r1
   r2 <- day2b
+  print "Day 2 b"
   print r2
   print (case r2 of (hor, depth, aim) -> hor * depth)
